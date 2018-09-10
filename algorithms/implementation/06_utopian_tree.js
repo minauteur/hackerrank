57264cb170cafe7f06e0f7d82f0f76058a996541
minauteur <Thompennington@gmail.com>
2018-09-10 17:42:24 -0500

    adds solution in JS

---
+++ algorithms/implementation/06_utopian_tree.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the utopianTree function below.
+function utopianTree(n) {
+    if (n === 0) {
+        return 1;
+    }
+    let period = 0;
+    let h = 1;
+    while (period <= n) {
+        if (period != 0 && period % 2 === 0) {
+            h = h + 1;
+        } else if (period % 2 != 0) {
+            h = h * 2;
+        }
+        period++
+    }
+    return h;
+}
+
+
+
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const t = parseInt(readLine(), 10);
+
+    for (let tItr = 0; tItr < t; tItr++) {
+        const n = parseInt(readLine(), 10);
+
+        let result = utopianTree(n);
+
+        ws.write(result + "\n");
+    }
+
+    ws.end();
+}
