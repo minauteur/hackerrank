be4b834e6673ec762192c69b917dc945807f7268
minauteur <Thompennington@gmail.com>
2018-09-10 18:27:03 -0500

    adds solution in JS

---
+++ algorithms/implementation/08_beautiful_days_at_the_movies.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the beautifulDays function below.
+function beautifulDays(i, j, k) {
+    let beautiful_days = [];
+    for (i; i <= j; i++) {
+        let s_r = parseInt(i.toString().split('').reverse().join(''));
+        if ((i - s_r) % k === 0) {
+            beautiful_days.length++
+        };
+    }
+    return beautiful_days.length;
+
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const ijk = readLine().split(' ');
+
+    const i = parseInt(ijk[0], 10);
+
+    const j = parseInt(ijk[1], 10);
+
+    const k = parseInt(ijk[2], 10);
+
+    let result = beautifulDays(i, j, k);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
