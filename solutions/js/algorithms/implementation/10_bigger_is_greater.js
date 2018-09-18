595e906f7beff7689e4cbe8e19216086c7503e5f
minauteur <Thompennington@gmail.com>
2018-09-18 13:23:45 -0500

    adds solutions in JS and C++14

---
+++ solutions/js/algorithms/implementation/10_bigger_is_greater.js
@@ -0,0 +1,105 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+// for a more thorough explanation of what's going on here,
+// see https://www.nayuki.io/page/next-lexicographical-permutation-algorithm
+// NOTE: 'PE:' in the comments below represents my effort to explain in plain english.
+// Complete the biggerIsGreater function below.
+function biggerIsGreater(w) {
+    var words = w.split('')
+    var i = 0;
+    var x;
+    var a;
+    var b;
+    var max_1 = -1;
+    var max_2;
+    var swap;
+    var answer = "";
+
+    // find the highest index i that s[i] < s[i+1]
+    // PE: While our index remains less than the length of the word,
+    // we iterate through each letter until we reach the latest letter
+    // that is "less" than the next, lexicographically, in the string.
+
+    while (i < words.length) {
+        if (words[i] < words[i + 1] && i > max_1) {
+            max_1 = i
+        }
+        i++;
+    }
+    // PE: if we didn't find any substrings of two meeting the 
+    // criteria, we return "no answer", because we've checked for
+    // the smallest possible lengths of substrings already (2), and no
+    // satisfying pairs existed;
+    if (max_1 == -1) {
+        return "no answer"
+    } else {
+
+        //find the highest index j > i that s[j] > s[i];
+        // PE: if a pair is found in the previous step, that represents our "lower bound"
+        // so we set max_1 to equal that, and pass along an offset to continue 
+        // iterating through the word, updating the offset and max_2 whenever we find
+        // letters meeting our criteria
+        x = max_1 + 1
+
+        while (x < words.length) {
+            if (words[x] > words[max_1]) {
+                max_2 = x
+            }
+            x++;
+        }
+        
+        //swap i & j
+        swap = words[max_1]
+        words[max_1] = words[max_2]
+        words[max_2] = swap
+
+        //reverse the order of all elements after index i;
+        for (a = 0; a <= max_1; a++) {
+            answer = answer.concat(words[a])
+        }
+
+        for (b = words.length - 1; b > max_1; b--) {
+            answer = answer.concat(words[b])
+        }
+        return answer
+    }
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const T = parseInt(readLine(), 10);
+
+    for (let TItr = 0; TItr < T; TItr++) {
+        const w = readLine();
+
+        let result = biggerIsGreater(w);
+
+        ws.write(result + "\n");
+    }
+
+    ws.end();
+}
--- solutions/rust/interview preparation kit/sorting/03_fraudulent_activity_notifications.rs
+++ solutions/rust/interview preparation kit/sorting/03_fraudulent_activity_notifications.rs
@@ -1,9 +1,7 @@
 use std::io::{self, Read};
-use std::collections::HashSet;
+use std::collections::VecDeque;
 use std::iter;
-impl<'a, T> WindowsMut<'a, T> {
-    fn next(&mut self) -> Option<&mut [T]> { ... }
-}
+
 fn main () {
     let mut input = String::new();
     let mut ans = String::new();
@@ -12,28 +10,17 @@ fn main () {
     
     let mut lines: Vec<&str> = input.split("\n").collect();
     let mut ln_iter = &mut lines.into_iter();
-    let params: Vec<&str> = ln_iter.next().unwrap().split_whitespace().collect();
+    let params: Vec<&str> = ln_iter.next().unwrap()
+        .split_whitespace()
+        .collect();
     let tx_num: usize = params[0].parse::<usize>().unwrap();
     let lk_bk: usize = params[1].parse::<usize>().unwrap();
-    let mut tx_list: Vec<u32> = ln_iter.next().unwrap().split_whitespace().map(|x|x.parse::<u32>().unwrap()).collect();
-    let mut windows = tx_list.as_slice().windows(lk_bk+1);
-    // eprintln!(
-    //     "number of transactions: {}\ndays to look back: {}",
-    //     tx_num, lk_bk
-    // );
-    // eprintln!("transaction list \n{:?}", tx_list);
-    let mut notifications = 0;
-    while let Some(i) = windows.next() {
-        {
-        let (hist,c_exp) = &i[...lk_bk,);
-        hist.sort();
-        let trigger = 2 * median(hist);
-            if trigger >= c_exp[0] { notifications = notifications+1; }
-        }
-    }
-    print!("{}", &notifications);
+    let mut tx_list: VecDeque<u32> = ln_iter.next().unwrap()
+        .split_whitespace()
+        .map(|x|x.parse::<u32>().unwrap())
+        .collect();
 }
-
+fn activityNotifications(vec: Vec<i32>)
 fn median(v: &[u32])->u32 {
     let count = v.len();
     if count % 2 == 1 {
