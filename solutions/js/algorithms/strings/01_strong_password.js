c33f906d5bc276ad167cdbffce818d45eabc5917
minauteur <Thompennington@gmail.com>
2018-09-19 14:17:15 -0500

    updates notes

--- solutions/js/algorithms/strings/01_strong_password.js
+++ solutions/js/algorithms/strings/01_strong_password.js
@@ -28,13 +28,15 @@ function minimumNumber(n, pass) {
     // the required groups of characters can be represented by
     // the following regex capture groups
     const c = [/[0-9]/, /[a-z]/, /[A-Z]/, /[!@#$%^&*()\-+"]/]
-        // then we map, using r to represent each test run in succession, 
-        // collecting tests that return true in an array
+        // then we map each group to r, running each test in succession, 
+        // before finally filtering for "truthy" values
+        // and collecting them in an array
         .map(r => !r.test(pass))
         .filter(Boolean).length
     // we then take the larger value between either the length of c
     // or else the difference between the required length of 6
-    // and the given password since we can assume any characters
+    // and the given password. Because we are only asked for the 
+    // "minimum" characters to add, we can assume any characters
     // added will satisfy remaining criteria as densely as possible.
     return Math.max(c, 6 - n);
 }
