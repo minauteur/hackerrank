c3ba084bec553850c294749abcd1d34e4c9608fc
minauteur <thompennington@gmail.com>
2020-01-22 15:20:13 -0600

    adds js solution

---
+++ solutions/js/032_making_candies.js
@@ -0,0 +1,32 @@
+//https://www.hackerrank.com/challenges/making-candies/problem
+function minimumPasses(m, w, p, n) {
+    let pass = 0,
+        min = Math.ceil(n / (m * w)),
+        points = 0;
+    while (pass < min) {
+        let dPass = Math.ceil((p - points) / (m * w));
+        pass += dPass;
+        points += m * w * dPass;
+        if (Math.floor(points / p) >= Math.abs(m - w)) {
+            points -= Math.abs(m - w) * p;
+            m > w ? w = m : m = w;
+            let upgrades = Math.floor(points / p);
+            if (upgrades > 0) {
+                if (upgrades % 2 === 0) {
+                    m = w = m + (upgrades / 2);
+                } else {
+                    m = w = m + Math.floor(upgrades / 2);
+                    m++;
+                }
+                points -= upgrades * p;
+            }
+        } else {
+            let upgrades = Math.floor(points / p);
+            m > w ? w += upgrades : m += upgrades;
+            points -= upgrades * p;
+        }
+        let minPass = Math.ceil((n - points) / (m * w));
+        if (min > minPass + pass) min = minPass + pass;
+    }
+    return min;
+}
