6de7fb9afbc9263547d65c8c7f4ba72608a2399a
Thom Pennington <Thompennington@gmail.com>
2023-12-19 12:14:41 -0600

    rm duplicate `main()`

--- solutions/rust/005_the_birthday_bar.rs
+++ solutions/rust/005_the_birthday_bar.rs
@@ -38,37 +38,6 @@ fn birthday(s: &[i32], d: i32, m: i32) -> i32 {
     return results;
 }
 
-
-
-fn main() {
-    let stdin = io::stdin();
-    let mut stdin_iterator = stdin.lock().lines();
-
-    let mut fptr = File::create(env::var("OUTPUT_PATH").unwrap()).unwrap();
-
-    let n = stdin_iterator.next().unwrap().unwrap().trim().parse::<i32>().unwrap();
-
-    let s: Vec<i32> = stdin_iterator.next().unwrap().unwrap()
-        .trim_end()
-        .split(' ')
-        .map(|s| s.to_string().parse::<i32>().unwrap())
-        .collect();
-
-    let first_multiple_input: Vec<String> = stdin_iterator.next().unwrap().unwrap()
-        .split(' ')
-        .map(|s| s.to_string())
-        .collect();
-
-    let d = first_multiple_input[0].trim().parse::<i32>().unwrap();
-
-    let m = first_multiple_input[1].trim().parse::<i32>().unwrap();
-
-    let result = birthday(&s, d, m);
-
-    writeln!(&mut fptr, "{}", result).ok();
-}
-
-
 fn main() {
     let stdin = io::stdin();
     let mut stdin_iterator = stdin.lock().lines();
